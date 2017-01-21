7454645bb468cccedc866e6646e31cb7e00adeda
Younger Jo <jo@novn.co>
2017-01-22 00:43:36 +0900

    Change alpha for vs when having items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -8,28 +8,24 @@ public class Inventory : MonoBehaviour {
 
 	private List<Item> items = new List<Item>();
 
-	public void AddItem(Item item) {
+	public bool AddItem(Item item) {
 		items.Add(item);
-		UpdateDisplays();
+		
+		foreach (ItemDisplay display in itemDisplays) {
+			if (display.isOn == false && item.colorType == display.colorType) {
+				display.isOn = true;
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	public void RemoveAllItems() {
 		items.Clear();
-		UpdateDisplays();
-	}
-
-	private void UpdateDisplays() {
-
+		
 		foreach (ItemDisplay display in itemDisplays) {
 			display.isOn = false;
-		}
-
-		foreach (Item item in items) {
-			foreach (ItemDisplay display in itemDisplays) {
-				if (item.colorType == display.colorType) {
-					display.isOn = true;
-				}
-			}
 		}
 	}
 }
