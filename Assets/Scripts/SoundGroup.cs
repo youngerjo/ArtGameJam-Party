5576e91a2c7efaa9c5cb251de098f5a8187bfc3d
Younger Jo <jo@novn.co>
2017-01-22 11:05:43 +0900

    Change title image

---
+++ Assets/Scripts/SoundGroup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundGroup {
+	
+	private string[] _clipNames;
+
+	public string[] clipNames {
+		get {
+			return _clipNames;
+		}
+	}
+
+	public SoundGroup(params string[] clipNames) {
+		_clipNames = clipNames;
+	}
+
+	string[] GetRandomSequence() {
+
+		List<string> names = new List<string>(clipNames);
+		List<string> shuffled = new List<string>();
+
+		while (names.Count > 0) {
+
+			int index = Random.Range(0, names.Count);
+			
+			string name = names[index];
+			shuffled.Add(name);
+
+			names.RemoveAt(index);
+		}
+
+		return shuffled.ToArray();
+	}
+}
--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour {
 
+	private class SequenceInfo {
+		public string name;
+		public string[] clipNames;
+		public AudioSource audioSource;
+		public int currentIndex = 0;
+	}
+
 	public static SoundPlayer shared;
+	public AudioClip[] audioClips;
 
 	private AudioSource[] audioSources;
+	private List<SequenceInfo> sequenceInfos = new List<SequenceInfo>();
+	
+	private Dictionary<string, SoundGroup> nameToGroups = new Dictionary<string, SoundGroup>();
 
 	void Awake() {
 		shared = this;
 		audioSources = GetComponentsInChildren<AudioSource>();
 	}
 
-	public void Play(string name, bool overlapped = false) {
+	void Update() {
+		foreach (SequenceInfo info in sequenceInfos) {
+			if (! info.audioSource.isPlaying) {
+				info.currentIndex++;
+				
+			}
+		}
+	}
 
-		if (! overlapped) {
+	public void StopAllLoop() {
 			foreach (AudioSource audioSource in audioSources) {
 				if (audioSource.loop && audioSource.name != name) {
 					audioSource.Stop();
 				}
 			}
+	}
+
+	public void Play(string name, bool overlapped = false) {
+
+		if (! overlapped) {
+			StopAllLoop();
 		}
 		
 		foreach (AudioSource audioSource in audioSources) {
@@ -30,6 +54,32 @@ public class SoundPlayer : MonoBehaviour {
 		}
 	}
 
+	public void PlayRandomSequence(string groupName, bool looped, bool overlapped = false) {
+
+		if (! nameToGroups.ContainsKey(groupName)) {
+			return;
+		}
+
+		if (! overlapped) {
+			StopAllLoop();
+		}
+
+		SoundGroup group = nameToGroups[groupName];
+		
+		GameObject channel = new GameObject("Audio Source");
+		channel.transform.parent = this.gameObject.transform;
+
+		AudioSource source = channel.AddComponent<AudioSource>();
+
+		SequenceInfo info = new SequenceInfo();
+		info.name = groupName;
+		info.clipNames = group.clipNames;
+		info.audioSource = source;
+		info.currentIndex = 0;
+
+		sequenceInfos.Add(info);
+	}
+
 	public void SetSourceVolume(string name, float volume) {
 
 		foreach (AudioSource audioSource in audioSources) {
@@ -37,5 +87,9 @@ public class SoundPlayer : MonoBehaviour {
 				audioSource.volume = volume;
 			}
 		}
+	}
+
+	public void RegisterGroup(string name, SoundGroup group) {
+		nameToGroups[name] = group;
 	}
 }
